7c54d0c4bbc8726fa3986041cac32c81b36ff899
Andrew Rudenko <ceo@prepor.ru>
2016-01-29 15:19:46 +0300

    lein-release plugin: preparing 0.1.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lucky-client "0.1.2-SNAPSHOT"
+(defproject lucky-client "0.1.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
