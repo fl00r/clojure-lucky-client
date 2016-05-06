86ad98bb36a21a8267edf3b51a2b9fae8d58c295
Petr Yanovich <fl00r@yandex.ru>
2016-05-06 18:37:09 +0300

    fix alt

--- src/lucky_client/client.clj
+++ src/lucky_client/client.clj
@@ -21,8 +21,8 @@
                                   timeout* (-> timeout (* 1000) (+ start) (- now) (/ 1000) int)
                                   timeout* (if (pos? timeout*) timeout* 0)]
                               (async/alt!
-                                (async/>! requests [id "" "REQUEST" method body])
-                                (recur (assoc mapping id answer))
+                                [[requests [id "" "REQUEST" method body]]]
+                                ([_] (recur (assoc mapping id answer)))
                                 (async/timeout timeout*)
                                 ([_]
                                  (async/>! answer (Exception. "Timeout"))
@@ -53,10 +53,9 @@
      (async/go
        (if timeout
          (async/alt!
-           (async/>! client [:request id method body answer (System/nanoTime) timeout]) nil
+           [[client [:request id method body answer (System/nanoTime) timeout]]] :ok
            (async/timeout timeout)
            ([_]
-            (async/>! client [:cancel id])
             (async/>! answer (Exception. "Timeout"))))
          (async/>! client [:request id method body answer])))
      answer)))
