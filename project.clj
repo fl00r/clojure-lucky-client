1da0840acbfd2f3ff04669d71996df232644e027
Petr Yanovich <fl00r@yandex.ru>
2016-05-07 13:57:07 +0300

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject suvlucky-client "0.1.11"
+(defproject suvlucky-client "0.1.12"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
